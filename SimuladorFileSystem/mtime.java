edf320a7ee19707e9ddfa0178b4d6d11596aa217
Fatake Raicos <pace_lozano@hotmail.com>
2020-03-19 15:39:51 -0600

    Cambios menores mtime

--- SimuladorFileSystem/mtime.java
+++ SimuladorFileSystem/mtime.java
@@ -10,41 +10,40 @@ public class mtime{
 		
 		Kernel.initialize();
 		
-		if(argv.length==0){
-			System.err.println( PROGRAM_NAME + ": usage: java " + PROGRAM_NAME + " input-file ------ NO HAY NINGUN ARGUMENTO" ) ;
+		if(argv.length == 0){
+			System.err.println( PROGRAM_NAME + ": usage: java " + PROGRAM_NAME + " input-file \n------ NO HAY NINGUN ARGUMENTO" ) ;
 			Kernel.exit( 1 ) ;
 		}
 		
 		
-	for( int i = 0 ; i < argv.length ; i ++ ){
-		File name = new File(argv[i]);
-		long ms = name.lastModified();
-		
-		System.out.println(ms);
-		
-		Date d = new Date(ms);
-		System.out.println(d);
-		Calendar c = new GregorianCalendar(); 
-		c.setTime(d);
-		
-		String dia = Integer.toString(c.get(Calendar.DATE));
-		String mes = Integer.toString(c.get(Calendar.MONTH)+1);
-		String annio = Integer.toString(c.get(Calendar.YEAR));
-		String hora = Integer.toString(c.get(Calendar.HOUR_OF_DAY));
-		String minuto = Integer.toString(c.get(Calendar.MINUTE));
-		String segundo = Integer.toString(c.get(Calendar.SECOND));
-		
-		System.out.println("Ultima fecha de modificacion del archivo: " + argv[i]);
-		System.out.println("DIA:" + dia +
-						   "  MES:" + mes +
-						   "  AÑO:" + annio + 
-						   "  HORA:" + hora + 
-						   "  MINUTO:" + minuto + 
-						   "  SEGUNDO:" + segundo);
-    }
+		for( int i = 0 ; i < argv.length ; i ++ ){
+			File name = new File(argv[i]);
+			long ms = name.lastModified();
+			
+			System.out.println(ms);
+			
+			Date d = new Date(ms);
+			System.out.println(d);
+			Calendar c = new GregorianCalendar(); 
+			c.setTime(d);
+			
+			String dia = Integer.toString(c.get(Calendar.DATE));
+			String mes = Integer.toString(c.get(Calendar.MONTH)+1);
+			String annio = Integer.toString(c.get(Calendar.YEAR));
+			String hora = Integer.toString(c.get(Calendar.HOUR_OF_DAY));
+			String minuto = Integer.toString(c.get(Calendar.MINUTE));
+			String segundo = Integer.toString(c.get(Calendar.SECOND));
+			
+			System.out.println("Ultima fecha de modificacion del archivo: " + argv[i]);
+			System.out.println("DIA:" + dia +
+							"  MES:" + mes +
+							"  AÑO:" + annio + 
+							"  HORA:" + hora + 
+							"  MINUTO:" + minuto + 
+							"  SEGUNDO:" + segundo);
+		}
 		Calendar calendar = Calendar.getInstance();
 		Date date = calendar.getTime();
 		System.out.println(date);
-		
 	}
 }
