0970753d7dc4b46bbc80252dff3c597e8b7f43a2
Fatake Raicos <pace_lozano@hotmail.com>
2020-03-20 10:13:30 -0600

    Cambios en ctime

--- SimuladorFileSystem/Ctime.java
+++ SimuladorFileSystem/Ctime.java
@@ -1,45 +1,27 @@
 import java.util.*;
 import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
 
-public class Ctime{
-	public static final String PROGRAM_NAME = "Atime" ;
-	
+
+public class ctime{
+	public static final String PROGRAM_NAME = "ctime" ;
 	public static void main(String[] argv) throws Exception{
 		Kernel.initialize();
-		
 		if(argv.length == 0){
-			System.err.println( PROGRAM_NAME + ": usage: java " + PROGRAM_NAME + " input-file \n------ NO HAY NINGUN ARGUMENTO" ) ;
+			System.err.println( PROGRAM_NAME + ": usage: java " + PROGRAM_NAME + " input-file ------ NO HAY NINGUN ARGUMENTO" ) ;
 			Kernel.exit( 1 ) ;
 		}
-		
-		for( int i = 0 ; i < argv.length ; i ++ ){
-			File name = new File(argv[i]);
-			long ms = name.lastModified();
-			
-			System.out.println(ms);
-			
-			Date d = new Date(ms);
-			System.out.println(d);
-			Calendar c = new GregorianCalendar(); 
-			c.setTime(d);
-			
-			String dia = Integer.toString(c.get(Calendar.DATE));
-			String mes = Integer.toString(c.get(Calendar.MONTH)+1);
-			String annio = Integer.toString(c.get(Calendar.YEAR));
-			String hora = Integer.toString(c.get(Calendar.HOUR_OF_DAY));
-			String minuto = Integer.toString(c.get(Calendar.MINUTE));
-			String segundo = Integer.toString(c.get(Calendar.SECOND));
-			
-			System.out.println("Ultima fecha de modificacion del archivo: " + argv[i]);
-			System.out.println("DIA:" + dia +
-							"  MES:" + mes +
-							"  AÑO:" + annio + 
-							"  HORA:" + hora + 
-							"  MINUTO:" + minuto + 
-							"  SEGUNDO:" + segundo);
-		}
-		Calendar calendar = Calendar.getInstance();
-		Date date = calendar.getTime();
-		System.out.println(date);
+	
+        for( int i = 0 ; i < argv.length ; i ++ ){
+            try{
+                System.out.println("ctime");
+                System.out.println(Files.getAttribute(new File(argv[i]).toPath(),"unix:ctime"));
+                System.out.println(new Date(((FileTime)Files.getAttribute(new File(argv[i]).toPath(),"unix:ctime")).toMillis()));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+    }
 	}
 }
