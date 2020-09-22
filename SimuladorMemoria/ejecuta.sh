a2e4ad0fa7294d864840d45fe34f0d3a3fa1267d
Fatake <pace_lozano@hotmail.com>
2020-09-22 18:09:46 -0500

    Cambios en el sh

--- SimuladorMemoria/ejecuta.sh
+++ SimuladorMemoria/ejecuta.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Compilando..."
 javac -d class MemoryManagement.java
+cp commands memory.conf /class
 echo "Ejecutando..."
 java -cp class MemoryManagement commands memory.conf
 #java -cp class MemoryManagement commands2 memory2.conf
